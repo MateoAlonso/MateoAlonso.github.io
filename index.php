e1b5423aeb47995d4ff00d88f31969f4faec1a6c
MateoAlonso <mateoalonsogioia27@gmail.com>
2021-05-10 20:30:38 -0300

    Added index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CV: Mateo Alonso Gioia</title>
+</head>
+
+<body class="fondo">
+    <div>
+        <h1>Este sitio está en desarrollo</h1>
+        <h1>This site is still in development</h1>
+    <h1>発展中</h1>
+    </div>
+    
+    <php ?>
+</body>
+
+</html>
